ed2ba007f0cfa2c5f41208294b9864e48d4ea4c0
Mirko627 <mirkoferrarimirkoferrari@gmail.com>
2026-08-27 15:30:37 +0200

    Add Exception Middleware

--- OfferService.API/Program.cs
+++ OfferService.API/Program.cs
@@ -138,6 +138,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseMiddleware<ExceptionMiddleware>();
+
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
